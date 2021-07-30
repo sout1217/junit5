23e57b49a1af5438576b662e5b04999e14aaccb3
hbh <sout1217@naver.com>
2021-07-30 12:27:51 +0900

    8강

--- src/test/java/com/inflearn/junit5/StudyTest.java
+++ src/test/java/com/inflearn/junit5/StudyTest.java
@@ -1,25 +1,26 @@
 package com.inflearn.junit5;
 
 
-import com.inflearn.junit5.annotations.FastTest;
-import com.inflearn.junit5.annotations.SlowTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StudyTest {
 
-    // 빨리 끝나는 테스트의 경우 로컬에서 테스트를 하길 원한다
-    @FastTest
-    @DisplayName("스터디 만들기 fast")
-    void assume_true_test() {
-        System.out.println("fast test");
+    @DisplayName("스터디 만들기")
+    @RepeatedTest(value = 10, name = "{displayName}, {currentRepetition}/{totalRepetitions}")
+    void test(RepetitionInfo repetitionInfo) {
+        System.out.println("현재: " + repetitionInfo.getCurrentRepetition() + "/" + repetitionInfo.getTotalRepetitions());
     }
 
-
-    // 오래걸리는 테스트의 경우 CI 에서 테스트를 하길 원한다
-    @SlowTest
-    @DisplayName("스터디 만들기 slow")
-    void slow_test() {
-        System.out.println("slow test");
+    // junit5 에서는 기본적으로 제공하며
+    // junit4 에서는 (서드파티) 라이브러리를 사용해야한다
+    @DisplayName("스터디 만들기")
+    @ParameterizedTest(name = "{index} {displayName} message={0}")
+    @ValueSource(strings = {"날씨가", "많이", "추워지고", "있네요"})
+    void test2(String message) {
+        System.out.println(message);
     }
-
 }
