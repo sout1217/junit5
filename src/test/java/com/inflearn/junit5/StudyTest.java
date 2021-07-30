fe35d1a40e60e8ddd48ccf51d5e0ad810fce8447
hbh <sout1217@naver.com>
2021-07-30 13:00:32 +0900

    10강

--- src/test/java/com/inflearn/junit5/StudyTest.java
+++ src/test/java/com/inflearn/junit5/StudyTest.java
@@ -2,83 +2,37 @@ package com.inflearn.junit5;
 
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.AggregateWith;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
-import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
-import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
-import org.junit.jupiter.params.converter.ArgumentConversionException;
-import org.junit.jupiter.params.converter.ConvertWith;
-import org.junit.jupiter.params.converter.SimpleArgumentConverter;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class StudyTest {
 
+    int value = 1;
+
+    // 메소드마다 서로다른 인스턴스 이기 때문에
+    // value 가 어떤 테스트에서든 1이다
+    // 테스트간의 의존성을 줄이기위해 새로운 인스턴스를 만드는 전략을 한 것 이다
+
+    // jUnit5 에서는 그나마 기본적으로 선언된 순서대로 실행은 되지만,
+    // 원래는 정해진 순서가 없어 코드가 많아지면 순서대로 실행이 되지 않을 때 가 있다
+
+    // 이러한 기본전략을 바꾸는 방법이 JUNIT5 에 추가 되었다
+    // 새로운 인스턴스를 계속 만들지 않기 때문에 조금이나마 성능 장점이 있다
+    // 테스트 인스턴스가 PER CLASS 인 경우 @BeforeAll 또는 @AfterAll 은 더 이상 static 으로 작성될 필요가 없다
+
+    @Test
     @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @ValueSource(strings = {"날씨가", "많이", "더워지고", "있네요"})
-    @NullAndEmptySource
-    void parameterTest(String message) {
-        System.out.println(message);
+    void create_study() {
+        System.out.println(value++);
+        System.out.println("test 1");
     }
 
+    @Test
     @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @ValueSource(ints = {10, 20, 40})
-    void parameterTest2(int limit) {
-        System.out.println(limit);
+    void create_study2() {
+        System.out.println(value++);
+        System.out.println("test 2");
     }
 
-    @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @ValueSource(ints = {10, 20, 40})
-    void parameterTest3(@ConvertWith(StudyConverter.class) Study study) {
-        System.out.println(study.getLimit());
-    }
-
-    // 1개의 argument 에만 적용 될 때 SimpleArgumentConverter
-    static class StudyConverter extends SimpleArgumentConverter {
-        @Override
-        protected Object convert(Object source, Class<?> targetClass) throws ArgumentConversionException {
-            assertEquals(Study.class, targetClass, "Can only convert to Study");
-            return new Study(Integer.parseInt(source.toString()));
-        }
-    }
-
-    @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
-    void parameterTest4(Integer limit, String name) {
-        Study study = new Study(limit, name);
-        System.out.println(study);
-    }
-
-    @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
-    void parameterTest5(ArgumentsAccessor  argumentsAccessor) {
-        Study study = new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
-        System.out.println(study);
-    }
-
-    // 여러개의 arg 를 할 때는 aggregate 를 사용
-    // inner static 클래스 또는 public 클래스 형태로 만들어야 한다
-    static class StudyAggregator implements ArgumentsAggregator {
-
-        @Override
-        public Object aggregateArguments(ArgumentsAccessor argumentsAccessor, ParameterContext parameterContext) throws ArgumentsAggregationException {
-            return new Study(argumentsAccessor.getInteger(0), argumentsAccessor.getString(1));
-        }
-    }
-    @DisplayName("스터디 만들기")
-    @ParameterizedTest(name= "{index} {displayName} message={0}")
-    @CsvSource({"10, '자바 스터디'", "20, 스프링"})
-    void parameterTest6(@AggregateWith(StudyAggregator.class) Study study) {
-        System.out.println(study);
-    }
 }
