08283f27433a284328a88b43ad8fd1e8fe47e629
hbh <sout1217@naver.com>
2021-07-30 12:11:18 +0900

    7강

--- src/test/java/com/inflearn/junit5/StudyTest.java
+++ src/test/java/com/inflearn/junit5/StudyTest.java
@@ -1,25 +1,23 @@
 package com.inflearn.junit5;
 
 
+import com.inflearn.junit5.annotations.FastTest;
+import com.inflearn.junit5.annotations.SlowTest;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 public class StudyTest {
 
     // 빨리 끝나는 테스트의 경우 로컬에서 테스트를 하길 원한다
-    @Test
+    @FastTest
     @DisplayName("스터디 만들기 fast")
-    @Tag("fast")
     void assume_true_test() {
         System.out.println("fast test");
     }
 
 
     // 오래걸리는 테스트의 경우 CI 에서 테스트를 하길 원한다
-    @Test
+    @SlowTest
     @DisplayName("스터디 만들기 slow")
-    @Tag("slow")
     void slow_test() {
         System.out.println("slow test");
     }
